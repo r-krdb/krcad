4483079e79033905d73445027dd9e732844c529a
chanyub Park <mrchypark@gmail.com>
2022-04-26 17:54:01 +0900

    add url list

---
+++ R/get.R
@@ -0,0 +1,32 @@
+url_list <- function() {
+  list(
+    `2016` = list(
+      data = "https://docs.google.com/spreadsheets/d/1i3CaZngtkLAIFYek2bfQOCJI2B4YRenhgJaPqfNf5no/edit?usp=sharing",
+      source = "https://www.assembly.go.kr/common/download.do?fid=communication&noticeid=1100021444&fpath=communication"
+    ),
+    `2017` = list(
+      data = "https://docs.google.com/spreadsheets/d/1y0wtlamIZ0S-sG1qaMEPd5wUmBrvdI37_A9DyfpRwOQ/edit?usp=sharing",
+      source = "https://www.assembly.go.kr/common/download.do?fid=communication&noticeid=1100021550&fpath=communication"
+    ),
+    `2018` = list(
+      data = "https://docs.google.com/spreadsheets/d/1VUO6V2FfMx86xrqLK7lWEGjHOiTVNYpj8LcKQQNdkF0/edit?usp=sharing",
+      source = "https://www.assembly.go.kr/common/download.do?fid=communication&noticeid=1100021715&fpath=communication"
+    ),
+    `2019` = list(
+      data = "https://docs.google.com/spreadsheets/d/1FnmxCWfqc4OQwN4rqi21DqprEoMpPEbQ4XgMmSHQ0Po/edit?usp=sharing",
+      source = "https://www.assembly.go.kr/common/download.do?fid=communication&noticeid=1100021876&fpath=communication"
+    ),
+    `2020` = list(
+      data = "https://docs.google.com/spreadsheets/d/1jMoZKX1RzVfOYW5prx4PSfFvA-S8orun_CSbDdYUbXA/edit?usp=sharing",
+      source = "https://www.assembly.go.kr/common/download.do?fid=communication&noticeid=1100022046&fpath=communication"
+    ),
+    `2021` = list(
+      data = "https://docs.google.com/spreadsheets/d/1XrVH3qa_hIGSn6aFjzQ6Zy7HfJKGj9vkIHAAoknde7o/edit?usp=sharing",
+      source = "https://www.assembly.go.kr/common/download.do?fid=communication&noticeid=1100022224&fpath=communication"
+    ),
+    `2022` = list(
+      data = "https://docs.google.com/spreadsheets/d/124rioS6kCtJrbuiXNATQmbrFVsBX2TTkQpsSDjco3jA/edit#gid=1563171242",
+      source = "https://www.assembly.go.kr/common/download.do?fid=communication&noticeid=1100022404&fpath=communication"
+    )
+  )
+}
